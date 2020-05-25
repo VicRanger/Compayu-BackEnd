ee1ef1baa2577133b6eba650a6dad27c02f7477a
VicRanger <wzzcjbb@163.com>
2020-05-25 18:30:28 +0800

    200524-2 by wzz 完成分布式channel配置

--- backsite/backsite/settings.py
+++ backsite/backsite/settings.py
@@ -57,11 +57,11 @@ MIDDLEWARE = [
 ]
 CHANNEL_LAYERS = {
     "default": {
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-        # "BACKEND": "channels_redis.core.RedisChannelLayer",
-        # "CONFIG": {
-        #     "hosts": [("127.0.0.1", 6379)],
-        # },
+        # 'BACKEND':'channels.layers.InMemoryChannelLayer'
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 # 
